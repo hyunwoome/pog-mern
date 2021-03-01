964cc65a31153b095f4ff24ec00cd3fac86fc38e
hyunwoome <hyunwoome@icloud.com>
2021-03-01 12:41:24 +0900

    Add 404Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Calendar from './pages/Calendar';
 import Menu from './pages/Menu';
 import Account from './pages/Account';
+import NotFound from './pages/NotFound';
 import BottomNav from './components/common/BottomNav';
 import Footer from './components/sections/Footer';
 
@@ -11,12 +12,13 @@ function App() {
 	return (
 		<>
 			<Route path="/" exact component={Home} />
-			<Route path="/login" exact component={Login} />
-			<Route path="/menu" exact component={Menu} />
-			<Route path="/account" exact component={Account} />
-			<Route path="/calendar" exact component={Calendar} />
-			<Footer />
+			<Route path="/login" component={Login} />
+			<Route path="/menu" component={Menu} />
+			<Route path="/account" component={Account} />
+			<Route path="/calendar" component={Calendar} />
+			<Route component={NotFound} />
 			<BottomNav />
+			<Footer />
 		</>
 	);
 }
---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default function NotFound() {
+	return <Container>페이지를 찾을 수 없습니다.</Container>;
+}
+
+const Container = styled.div`
+	height: 90vh;
+`;
